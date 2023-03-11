42b77f5a776e8e601831812f97a9c8d75340fb20
Daniel-Iosif Trubacs <92638091+dntrubacs@users.noreply.github.com>
2023-03-11 14:56:04 +0000

    Update LoadWPL.py
    Changed the file to return the required tracks (list of waypoints) from .txt file

--- LoadWPL.py
+++ LoadWPL.py
@@ -1,19 +1,22 @@
 """
 Created by Daniel-Iosif Trubacs for the MAC team on 8 March 2023. The purpose of this module is to load an
-array of Waypoints from .txt file.txt The data in .txt file should be in the NMEA format.
+array of Waypoints from a .txt and arrange them in different tracks. A track should be a simple numpy array
+containing the waypoints (in order) that should be followed). The data in .txt file should be in the NMEA format.
 """
 
 import numpy as np
 
 
-def load_wpl(txt_file: str) -> np.ndarray:
-    """ Loads a series of waypoint from a .txt file.
+def load_wpl(txt_file: str) -> list:
+    """ Loads a series of waypoint from a .txt file and return the track expected to be followed.
 
     Args:
         txt_file: path to data file (waypoints must be in the NMEA format). An example of
-                  a waypoint is: $MMWPL,5050.710799,N,00044.755897,W,WPT 1.
+                  a waypoint is: $MMWPL,5050.710799,N,00044.755897,W,WPT 1. The waypoints will
+                  be then arranged in different tracks according to the given commands. An example of
+                  of a set track command is: '$MMRTE,2,2,c,TRACK 1,WPT 6,WPT 7,WPT 8'
     Returns:
-        array containing the list of waypoints values
+        (n_tracks, n_waypoints, lat, lon): list containing the list of waypoints values arranged in tracks as numpy arrays
     """
     # read the .txt file
     f = None
@@ -30,18 +33,42 @@ def load_wpl(txt_file: str) -> np.ndarray:
         # the array containing the waypoints
         waypoints = []
 
+        # array containing the names of waypoints (e.g 'WPT 1')
+        #  the name of waypoints[i] is given by waypoints_name[i]
+        waypoints_name = []
+
         # load the waypoints
         for line in lines:
             # check whether the command starts with '$WMPL'
             line_split = line.split(',')
             if line_split[0] == '$MMWPL':
-                # append the waypoint data (latitude and longitude)
-                waypoints.append([float(line_split[1]), float(line_split[3])])
+                # append the waypoint data (latitude, longitude, waypoint_index)
+                waypoints.append(np.array([float(line_split[1]), float(line_split[3])]))
+                waypoints_name.append(line_split[5].strip())
 
-        # return the numpy array containing the waypoints
-        return np.array(waypoints)
+        # the array containing all the tracks
+        tracks = []
+
+        # go over all the tracks and extract the required waypoints
+        # load the waypoints
+        for line in lines:
+            # check whether the command starts with '$MMRTE'
+            line_split = line.split(',')
+            if line_split[0] == '$MMRTE':
+                # the current waypoints that make up the current track
+                track_wp_name = [line_split[i].strip() for i in range(len(line_split)) if line_split[i].strip()
+                                 in waypoints_name]
+
+                # the current track containing the waypoints
+                current_track = np.array([waypoints[waypoints_name.index(x)] for x in track_wp_name])
+
+                # add the current track to the list of tracks
+                tracks.append(current_track)
+
+        # return the numpy array containing the tracks
+        return tracks
 
 
 if __name__ == '__main__':
-    waypoints_test = load_wpl('data.txt')
-    print(waypoints_test)
+    tracks_test = load_wpl('data.txt')
+    print(tracks_test[0][0])
