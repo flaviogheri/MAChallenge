90d73519db03385ac40a46f02c18c7b8947afb02
flaviogheri <gheriarnaudflavio@gmail.com>
2023-03-11 20:09:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from LoadWPL import load_wpl
 
 if __name__ == '__main__':
     data_file =  load_wpl('data.txt')
-    print(tracks_test[0][0])
+    print(data_file)
 
-sim = Simulator.simulate(data_file)
+sim = boat_similator.simulate(data_file)
 print("/////////----------------")
