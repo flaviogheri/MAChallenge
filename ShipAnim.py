48250ba27eecb509fb0555f3f04968d40719e72c
Daniel-Iosif Trubacs <92638091+dntrubacs@users.noreply.github.com>
2023-03-16 22:01:05 +0000

    yo

--- ShipAnim.py
+++ ShipAnim.py
@@ -6,10 +6,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def find_limits(initial_position: np.ndarray, waypoints: np.ndarray) -> np.ndarray:
+    """Creates limits for the plots
+
+    Args:
+        initial_position: the initial position of the boat (lat, lon) in DEG format
+        waypoints: list of all waypoints in DEG format
+
+    Returns: [[lon_min, lon_max], [lat_min,lat_max]]
+
+    """
+    # the range for lat and long for the waypoints and add 10% to it
+    range_list = np.concatenate((waypoints, np.expand_dims(initial_position, axis=0)))
+
+    lat_range = 3.3 * (np.max(range_list[:, 0]) - np.min(range_list[:, 0]))
+    lon_range = 3.3 * (np.max(range_list[:, 1]) - np.min(range_list[:, 1]))
+
+    # lat limits for the plot and add 15%
+    lat_limit = np.array([np.max(range_list[:, 0]) - lat_range / 2, np.min(range_list[:, 0]) + lat_range / 2])
+    lon_limit = np.array([np.max(range_list[:, 1]) - lon_range / 2, np.min(range_list[:, 1]) + lon_range / 2])
+
+    return np.array([lon_limit, lat_limit])
+
+
 def set_plot(waypoints: np.ndarray, current_pos: np.ndarray, current_speed: float,
-             current_err:float, limits:np.ndarray, axis):
+             current_err:float, limits:np.ndarray, current_heading: float,
+             path: np.ndarray, axis):
     """ Draws the waypoints on interactive plot. Latitude is plotted on the y axis and longitude is plotted on
-        the x axis.
+        the x axis. All lat and lon values should be in DEG format.
 
     Args:
         waypoints: numpy array representing list of waypoints (n_waypoints, lat ,long)
@@ -17,9 +41,14 @@ def set_plot(waypoints: np.ndarray, current_pos: np.ndarray, current_speed: floa
         current_speed: current speed of the bloat
         current_err: current cross track error in relation to the current and last waypoint
         limits: lat and long limits of the current plot [[lat_min,lat_max], [long_min, long_max]]
+        current_heading: current heading of the boat
+        path: the path followed by the boat should, ([past_lat_pos, past_lon_positions])
         axis: matplotlib object representing the axis on which the data is plotted (axis.plot)
 
     """
+    # waypoint range used to for correct location of plotting
+    waypoint_range = [limits[0][1] - limits[0][0], limits[1][1] - limits[1][0]]
+
     # set the label of the plot
     axis.set_xlabel('Longitude')
     axis.set_ylabel('Latitude')
@@ -30,19 +59,30 @@ def set_plot(waypoints: np.ndarray, current_pos: np.ndarray, current_speed: floa
 
     # plotting the waypoints
     for i in range(len(waypoints)):
-        axis.plot(waypoints[i][1], waypoints[i][0], marker='o', markersize=10)
-        axis.text(waypoints[i][1], waypoints[i][0], 'WP'+ str(i+1))
+        axis.plot(waypoints[i][1], waypoints[i][0], marker='x', markersize=10)
+        axis.text(waypoints[i][1], waypoints[i][0], 'WP'+str(i+1))
 
-    waypoint_range = [limits[0][1] - limits[0][0], limits[1][1] - limits[1][0]]
-
-    #axis.text(waypoints[0, 1] + waypoint_range[1] * 0.1, waypoints[0, 0] + waypoint_range[0] * 0.05, f"Heading: {heading:.2f} rad")
-    axis.text(waypoints[0, 1] - waypoint_range[1] * 0.25, waypoints[0, 0] + waypoint_range[0] * 0.25, f"Speed: {current_speed:.2f} kts")
-    axis.text(waypoints[0, 1] - waypoint_range[1] * 0.25, waypoints[0, 0] + waypoint_range[0] * 0.275, f"Cross Track Error: {current_err:.2f} m")
-    #print(waypoints[0, 1] + waypoint_range[1] * 0.0, waypoints[0, 0] + waypoint_range[0] * 0.0)
-    #print(limits, waypoints[0, 1] + waypoint_range[1] * 0.6)
     # plotting the current position
-    #axis.plot(current_pos[1], current_pos[0], color = 'r', markersize=10, marker='1')
-    #print(limits[1][1]*0.9999, limits[0][1]*0.9999)
-    # Redraw the plot and pause briefly to allow the plot to update
+    axis.plot(current_pos[1], current_pos[0], color = 'r', markersize=10, marker='1')
+    axis.text(current_pos[1]*1.0002, current_pos[0]*1.000004, 'Position:'+str(round(current_pos[0], 6))+'$^o$ '
+              +str(round(current_pos[1], 6))+'$^o$')
+
+    # showing the speed
+    axis.text(waypoints[0, 1] - waypoint_range[1] * 0.25, waypoints[0, 0] + waypoint_range[0] * 0.25,
+              'Speed:'+str(round(current_speed, 3)))
+
+    # showing the heading (in degrees)
+    axis.text(current_pos[1]*1.0002, current_pos[0]*1.000001, 'Heading:'+str(round(current_heading, 3)))
+
+    # showing the current track error (in degrees)
+    axis.text(waypoints[0, 1] - waypoint_range[1] * 0.25, waypoints[0, 0] + waypoint_range[0] * 0.275,
+              'CT error:' + str(round(current_err, 3)))
+
+    # show the path followed by the boat
+    axis.plot(path[1], path[0], lw=1, markersize=10, color='black')
+
+    # invert x axis as longitude in the west
+    plt.gca().invert_xaxis()
+
 
 
