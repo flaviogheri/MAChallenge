e61a2a05073073ce8d934e8626f9788eb849db2f
Nefelie <53905356+Nefelie@users.noreply.github.com>
2023-03-17 02:26:22 +0000

    final changes

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 """runs the main code"""
-from boat_simulator import Simulator
-from boat_simulator2 import local_Simulator
+# from boat_simulator import Simulator
+# from boat_simulator2 import local_Simulator
+from boat_simulator_trial import Simulator
 
 sim = Simulator('data.txt')
 #sim = local_Simulator('data.txt')
 #sim.simulate()
 sim.simulate()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
