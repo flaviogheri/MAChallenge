0658afbcca375f0901fbaff1fd708719800a810d
Daniel-Iosif Trubacs <92638091+dntrubacs@users.noreply.github.com>
2023-03-17 00:59:26 +0000

    proper code

--- testsimulator.py
+++ testsimulator.py
@@ -1,9 +1,5 @@
-""" Created by Daniel-Iosif Trubacs and Flavio Gheri for the MAChellenge on 16 March 2023. The main aim of
-this module is to create visualization for the ship movement.
-"""
-
+import matplotlib.pyplot as plt
 import numpy as np
-<<<<<<< HEAD
 from ShipAnim import set_plot, find_limits
 from LoadWPL import load_wpl
 from LOS_guidance import DMM_to_DEG
@@ -38,34 +34,26 @@ initial_pos = np.array(DMM_to_DEG(np.array([5050.708799, 44.755897])))
 
 # the limits for the plot
 pl_limits = find_limits(initial_position=initial_pos, waypoints=waypoints_list)
-=======
-from matplotlib import pyplot as plt
 
+# Initialize the time and frequency variables
+t = 0
+freq = 1
 
-def find_limits(initial_position: np.ndarray, waypoints: np.ndarray) -> np.ndarray:
-    """Creates limits for the plots
+# Define a function to handle the keyboard interrupt event
+def on_key_press(event):
+    if event.key == 'p':
+        fig.canvas.stop_event_loop()
 
-    Args:
-        initial_position: the initial position of the boat (lat, lon) in DEG format
-        waypoints: list of all waypoints in DEG format
+# Create an empty plot with axis labels
+fig, ax = plt.subplots()
+plt.ion()
 
-    Returns: [[lon_min, lon_max], [lat_min,lat_max]]
->>>>>>> 0db704eff3d95ca19947df0d05b6066cf9d7b79f
+# Connect the keyboard interrupt handler to the plot window
+fig.canvas.mpl_connect('key_press_event', on_key_press)
+""" Created by Daniel-Iosif Trubacs and Flavio Gheri for the MAChellenge on 16 March 2023. The main aim of
+this module is to create visualization for the ship movement.
+"""
 
-    """
-    # the range for lat and long for the waypoints and add 10% to it
-    range_list = np.concatenate((waypoints, np.expand_dims(initial_position, axis=0)))
-
-    lat_range = 3 * (np.max(range_list[:, 0]) - np.min(range_list[:, 0]))
-    lon_range = 3 * (np.max(range_list[:, 1]) - np.min(range_list[:, 1]))
-
-    # lat limits for the plot and add 15%
-    lat_limit = np.array([np.max(range_list[:, 0]) - lat_range / 2, np.min(range_list[:, 0]) + lat_range / 2])
-    lon_limit = np.array([np.max(range_list[:, 1]) - lon_range / 2, np.min(range_list[:, 1]) + lon_range / 2])
-
-    return np.array([lon_limit, lat_limit])
-
-<<<<<<< HEAD
 # Continuously generate new data and update the plot
 past_lat = []
 past_lon = []
@@ -73,7 +61,6 @@ past_lon = []
 while True:
     #plt.annotate("", xy=(data_x[t]+np.cos(theta[t]), data_y[t]+np.sin(theta[t])), xytext=(data_x[t], data_y[t]),
                 #arrowprops=dict(arrowstyle="->"))
-
 
     if t == 0:
      set_plot(waypoints=waypoints_list, current_pos = np.array([lat_data[0], lon_data[-1]]), current_speed=speed[t],
@@ -88,74 +75,23 @@ while True:
     track = np.array([past_lat, past_lon])
 
 
-=======
 
-def set_plot(waypoints: np.ndarray, current_pos: np.ndarray, current_speed: float,
-             current_err: float, limits: np.ndarray, current_heading: float,
-             path: np.ndarray, axis):
-    """ Draws the waypoints on interactive plot. Latitude is plotted on the y axis and longitude is plotted on
-        the x axis. All lat and lon values should be in DEG format.
->>>>>>> 0db704eff3d95ca19947df0d05b6066cf9d7b79f
+    plt.draw()
+    plt.pause(0.01)
 
-    Args:
-        waypoints: numpy array representing list of waypoints (n_waypoints, lat ,long)
-        current_pos: current position of the bloat (lat, long)
-        current_speed: current speed of the bloat
-        current_err: current cross track error in relation to the current and last waypoint
-        limits: lat and long limits of the current plot [[lat_min,lat_max], [long_min, long_max]]
-        current_heading: current heading of the boat
-        path: the path followed by the boat should, ([past_lat_pos, past_lon_positions])
-        axis: matplotlib object representing the axis on which the data is plotted (axis.plot)
+    # Increment the time variable
+    t += 1
 
-    """
-    # waypoint range used to for correct location of plotting
-    waypoint_range = [limits[0][1] - limits[0][0], limits[1][1] - limits[1][0]]
+    # Check if a keyboard interrupt event has occurred
+    if not plt.fignum_exists(fig.number):
+        break
 
-    # set the label of the plot
-    axis.set_xlabel('Longitude')
-    axis.set_ylabel('Latitude')
+    # Clear the plot to allow for a live update
+    ax.cla()
 
-    # set the limits of the plot
-    plt.xlim(limits[0])
-    plt.ylim(limits[1])
-
-<<<<<<< HEAD
 
 # Turn off interactive plotting
 plt.ioff()
-=======
-    # plotting the waypoints
-    for i in range(len(waypoints)):
-        axis.plot(waypoints[i][1], waypoints[i][0], marker='x', color='red',  markersize=10)
-        axis.text(waypoints[i][1], waypoints[i][0], 'WP' + str(i + 1))
-        
-    # plot a dashed line between the waypoints
-    axis.plot(waypoints[:, 1], waypoints[:, 0], color='red', lw=1, linestyle='dashed' ) 
-    
-    # plotting the current position
-    axis.plot(current_pos[1], current_pos[0], color='blue', markersize=5, marker='o')
-    plt.title('Position: ' + str(round(current_pos[0], 6)) + '$^o$ '
-              + str(round(current_pos[1], 6)) + '$^o$')
->>>>>>> 0db704eff3d95ca19947df0d05b6066cf9d7b79f
 
-    # showing the speed
-    axis.text(waypoints[0, 1] + waypoint_range[1] * 0.4, waypoints[0, 0] + waypoint_range[0] * 0.25,
-              'Speed: ' + str(round(current_speed, 3))+ ' kts')
-
-    # showing the current track error (in degrees)
-    axis.text(waypoints[0, 1] + waypoint_range[1] * 0.4, waypoints[0, 0] + waypoint_range[0] * 0.275,
-              'CT error: ' + str(round(current_err, 3)) + ' m')
-
-    # showing the heading (in degrees)
-    axis.text(waypoints[0, 1] - waypoint_range[1] * 1.2, waypoints[0, 0] + waypoint_range[0] * 0.275,
-              'Heading:' + str(round(current_heading, 3)) + '$^o$')
-
-    # draw an arrow pointing to the heading
-    plt.annotate("", xy=(current_pos[1] + 0.0001*np.cos(current_heading), current_pos[0] + 0.0001*np.sin(current_heading)),
-                 xytext=(current_pos[1], current_pos[0]), arrowprops=dict(arrowstyle="->"))
-
-    # show the path followed by the boat
-    axis.plot(path[1], path[0], lw=2, markersize=10, color='black')
-
-    # invert x axis as longitude in the west
-    plt.gca().invert_xaxis()
+# Show the final plot
+plt.show()
