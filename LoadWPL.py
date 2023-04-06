2c744961e812280f89563f70ebe24c8b50c90a11
Daniel-Iosif Trubacs <92638091+dntrubacs@users.noreply.github.com>
2023-04-06 17:12:30 +0100

    Added the update_log function to save data into a log file

--- LoadWPL.py
+++ LoadWPL.py
@@ -5,6 +5,8 @@ containing the waypoints (in order) that should be followed). The data in .txt f
 """
 
 import numpy as np
+import os
+import yaml
 
 
 def load_wpl(txt_file: str) -> list:
@@ -67,6 +69,54 @@ def load_wpl(txt_file: str) -> list:
 
         # return the numpy array containing the tracks
         return tracks
+
+
+def update_boat_log(recorded_time: float, recorded_speed: float, recorded_heading: float, recorded_latitude: float,
+                    recorded_longitude: float, file_name: str = 'boat_data_log.yaml'):
+    """ Saves the speed, heading, position (and the time they were recorded) into a yaml file.
+
+    Args:
+        recorded_time: the time at which this data was recorded.
+        recorded_speed: recorded speed of the boat by the external hardware.
+        recorded_heading: recorded heading of the boat by the external hardware.
+        recorded_latitude: recorded latitude of the boat by the external hardware.
+        recorded_longitude: recorded longitude of the boat by the external hardware.
+        file_name: the name of tha yaml file on which the data is recorded. Defaults to
+        'boat_data_log.yaml'
+
+    Returns:
+
+    """
+    # if the file exists, load the variables into boat_data_log
+    if os.path.exists(file_name):
+        # Open the YAML file containing the data for boat setup.
+        with open(file_name, "r") as f:
+            # Use the load() function to load the file contents into a Python object
+            boat_data_log = yaml.load(f, Loader=yaml.FullLoader)
+
+    # if the file doesn't exist, create the boat_data_log variable
+    else:
+        # Define the initial data for the YAML file
+        boat_data_log = {
+            "time_log": [],
+            "speed_log": [],
+            "heading_log": [],
+            "position_log": {
+                "latitude": [],
+                "longitude": []
+            }
+        }
+
+    # append the data to the log
+    boat_data_log["time_log"].append(recorded_time)
+    boat_data_log["speed_log"].append(recorded_speed)
+    boat_data_log["heading_log"].append(recorded_heading)
+    boat_data_log["position_log"]['latitude'].append(recorded_latitude)
+    boat_data_log["position_log"]['longitude'].append(recorded_longitude)
+
+    # Write the updated data to the YAML file
+    with open(file_name, "w") as yaml_file:
+        yaml.dump(boat_data_log, yaml_file)
 
 
 if __name__ == '__main__':
